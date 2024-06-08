e31be2c9135fb4415b4396bff6b13f390d12b83a
Amit <amitroy9546184327@gmail.com>
2024-06-08 13:37:13 +0530

    String Concatenation

--- string.js
+++ string.js
@@ -1,9 +1,14 @@
-// String interpolation
+// String interpolation --- Automatic replacing of variables with real values is called string interpolation.
 
 const name = "amit"
 const repoCount = 3
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+
+// String Concatenation 
+let fName = "Raj"
+let age = 32
+console.log("My name is "+fName+" And my age is "+age+".");
 
 let gameName = new String('amit-pal-com')
 // console.log(gameName[0]);
@@ -22,9 +27,9 @@ const newString = gameName.substring(0,4) // 0 to (4-1)
 // const anotherString = gameName.slice()
 // console.log(anotherString);
 
-const newStringOne = "       amit       "
+const newStringOne = "       amit pal       "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim()); // trim remove all whitespaces(before and after the string) from string
 
 
 const url = "https://amitpal.com/amit%20pal"
