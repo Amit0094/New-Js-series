c4e81b353e0c324183f802ba096c4cf975352fef
Amit <amitroy9546184327@gmail.com>
2024-06-10 00:07:59 +0530

    object_part-2

---
+++ 02_basics/object_part-2.js
@@ -0,0 +1,62 @@
+// const tinderUser = new Object() // this is a singleton object
+const tinderUser = {} // this is a non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammi"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const reguralUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Amit",
+            lastname: "Pal"
+        }
+    }
+}
+
+// console.log(reguralUser.fullname.userfullname.firstname);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+const obj3 = { 5: "e", 6: "f" }
+
+// const obj4 = {obj1,obj2}
+// const obj4 = Object.assign({}, obj1, obj2,obj3)
+
+const obj4 = { ...obj1, ...obj2, ...obj3 }
+// console.log(obj4);
+
+
+const users = [
+    {
+        id: 1,
+        email: "amit@gmail.com"
+    },
+    {
+        id: 1,
+        email: "amit@gmail.com"
+    },
+    {
+        id: 1,
+        email: "amit@gmail.com"
+    },
+]
+
+// console.log(users[1].email); // amit@gmail.com
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); // return an array of keys like this -> { [ 'id', 'name', 'isLoggedIn' ] }
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // Returns an array of key/values of the enumerable properties of an object
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
