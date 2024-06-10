9a393a4cd0c2296e03419b62cb6acef361e9c392
Amit <amitroy9546184327@gmail.com>
2024-06-10 14:32:43 +0530

    rest operator

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -32,5 +32,47 @@ function loginUserMessage(username = "Sam"){ // sam is given as a bydefault valu
 }
 
 // console.log(loginUserMessage("Amit"));
-console.log(loginUserMessage()); // when we not pass any argument in funcion then it return undefined -> { undefined just logged in }
+// console.log(loginUserMessage()); // when we not pass any argument in funcion then it return undefined -> { undefined just logged in }
+
+
+
+
+
+
+// when we don't know about the number of argument
+
+function calculateCartPrice(val1 , val2 , ...num1){ // ... rest operator , simply return an array of element of argument which is passed during function call
+    return num1
+}
+
+// console.log(calculateCartPrice(200,100,300,400)); // [ 200, 100, 300, 400 ]
+
+// console.log(calculateCartPrice(100,200,300,400,500)); // [ 300, 400, 500 ] because 100 and 200 assigned in val1 and val2 respectively..
+
+
+// object passed as an argument
+
+const user = {
+    username : "amit",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username : "Sam",
+    price : 399
+})
+
+// array passed as an argument
+
+const myNewArray  = [200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([9,99,999]));
 
