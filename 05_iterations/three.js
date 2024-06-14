3e9f5b42d693cf195dc1902e26ec8d2659a5d04d
Amit <amitroy9546184327@gmail.com>
2024-06-14 13:48:17 +0530

    for-of_loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,44 @@
+// for-of loop
+
+// [{},{},{}]
+// ["","",""]
+
+const arr = [1, 2, 3, 4, 5]
+
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     if (greet == " " || greet == "!") {
+//         continue
+//     }
+//     console.log(`Each char is : ${greet}`);
+// }
+
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key , ':-' , value);
+// }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'spiderman'
+}
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
